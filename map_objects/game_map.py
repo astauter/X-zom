b982ccd6298bd91f0b7652d9cdb60b54720da979
Alexander <ajstauter@gmail.com>
2019-01-07 13:09:40 -0600

    randomly generated dungeon

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
 
@@ -22,13 +24,52 @@ class GameMap:
         #presetting tiles for demonstration
         return tiles
 
-    def make_map(self):
-        #create two rooms to demonstrate
-        room1 = Rect(20, 15, 10, 15)
-        room2 = Rect(35, 15, 10, 15)
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+        rooms = []
+        num_rooms = 0
 
-        self.create_room(room1)
-        self.create_room(room2)
+        for r in range(max_rooms):
+            #random width and height
+            w = randint(room_min_size, room_max_size)
+            h = randint(room_min_size, room_max_size)
+
+            # random position of room without going out of the boundaries of the map
+            x = randint(0, map_width - w - 1)
+            y = randint(0, map_height - h -1)
+
+            new_room = Rect(x, y, w, h)
+
+            #run through the other rooms and see if they intersect with this one
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    break
+                #so if the loop finds that the rooms find an intersect we break out
+            else:
+                #if the loop was allowed to finish without finding any intersections, i.e. the room is valid
+
+                self.create_room(new_room)
+                (new_x, new_y) = new_room.center()
+
+                if num_rooms == 0:
+                    #first room where player starts at
+                    player.x = new_x
+                    player.y = new_y
+                else:
+                    #all rooms after the first connect it to the previous room with a tunnel using the center coordinates of the previous room
+                    (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                    #flip a coin
+                    if randint(0, 1) == 1:
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+
+                #add the new room to our list/array
+                rooms.append(new_room)
+                num_rooms += 1
+
 
     def create_room(self, room):
         # got through the tiles in the created rectangle and make them passagble
@@ -37,6 +78,16 @@ class GameMap:
                 self.tiles[x][y].blocked = False
                 self.tiles[x][y].block_sight = False
         #x1 +1 are used because we need to account for walls makes the rectangles 1 smaller then they seem, pythons range function does not include the end value in its range
+
+    def create_h_tunnel(self, x1, x2, y):
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:

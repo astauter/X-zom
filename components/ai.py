eea906efdb954c538c560e7f6887b3c957c054c4
Alexander <ajstauter@gmail.com>
2019-01-10 20:18:29 -0600

    death added

--- components/ai.py
+++ components/ai.py
@@ -3,6 +3,8 @@ import libtcodpy as libtcod
 
 class BasicMonster:
     def take_turn(self, target, fov_map, game_map, entities):
+        results = []
+
         monster = self.owner
         if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
 
@@ -10,5 +12,7 @@ class BasicMonster:
                 monster.move_astar(target, entities, game_map)
 
             elif target.fighter.hp > 0:
-                print('The {0} insults you! Your ego is damaged'.format(
-                    monster.name))
+                attack_results = monster.fighter.attack(target)
+                results.extend(attack_results)
+
+        return results
--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import libtcodpy as libtcod
 
 from components.fighter import Fighter
+from death_functions import kill_monster, kill_player
 from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
 from game_states import GameStates
@@ -97,6 +98,8 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
+        player_turn_results = []
+
         if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
             destination_x = player.x + dx
@@ -112,8 +115,9 @@ def main():
                         print('Hello Bob!')
 
                     else:
-                        print('You kick the ' + target.name +
-                              ' in the shins, dealing 0! damage!')
+                        attack_results = player.fighter.attack(target)
+                        player_turn_results.extend(attack_results)
+
                 else:
                     player.move(dx, dy)
 
@@ -127,12 +131,50 @@ def main():
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
+        for player_turn_result in player_turn_results:
+            message = player_turn_result.get('message')
+            dead_entity = player_turn_result.get('dead')
+
+            if message:
+                print(message)
+
+            if dead_entity:
+                if dead_entity == player:
+                    message, game_state = kill_player(dead_entity)
+                else:
+                    message = kill_monster(dead_entity)
+
+                print(message)
+
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
                 if entity.ai:
-                    entity.ai.take_turn(player, fov_map, game_map, entities)
+                    enemy_turn_results = entity.ai.take_turn(
+                        player, fov_map, game_map, entities)
 
-            game_state = GameStates.PLAYERS_TURN
+                    for enemy_turn_result in enemy_turn_results:
+                        message = enemy_turn_result.get('message')
+                        dead_entity = enemy_turn_result.get('dead')
+
+                        if message:
+                            print(message)
+
+                        if dead_entity:
+                            if dead_entity == player:
+                                message, game_state = kill_player(dead_entity)
+                            else:
+                                message = kill_monster(dead_entity)
+
+                            print(message)
+
+                            if game_state == GameStates.PLAYER_DEAD:
+                                break
+
+                        if game_state == GameStates.PLAYER_DEAD:
+                            break
+
+            else:
+                game_state = GameStates.PLAYERS_TURN
 
         #key = libtcod.console_check_for_keypress()
         # if key.vk == libtcod.KEY_ESCAPE:
